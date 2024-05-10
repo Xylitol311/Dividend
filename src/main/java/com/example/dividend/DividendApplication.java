72a989483958bb6b8f392f93b6530f97377a2945
Xylitol311 <soohong0311@gmail.com>
2024-05-10 16:18:03 +0900

    chore: first 세팅

--- src/main/java/com/example/dividend/DividendApplication.java
+++ src/main/java/com/example/dividend/DividendApplication.java
@@ -1,13 +1,46 @@
 package com.example.dividend;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class DividendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DividendApplication.class, args);
+//        SpringApplication.run(DividendApplication.class, args);
+
+        try {
+            Connection connection = Jsoup.connect("https://finance.yahoo.com/quote/COKE/history?guccounter=1&period1=99153000&period2=1715240712&frequency=1mo");
+            connection.header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36");
+            Document document = connection.get();
+            Elements eles = document.getElementsByAttributeValue("data-testid", "history-table");
+            Element ele = eles.get(0); // table 전체
+
+            Element tbody = ele.children().get(2).children().get(0).children().get(1);
+            for (Element e : tbody.children()) {
+                String txt = e.text();
+                if (!txt.endsWith("Dividend")) {
+                    continue;
+                }
+                String[] splits = txt.split(" ");
+                String month = splits[0];
+                int day = Integer.valueOf(splits[1].replace(",", ""));
+                int year = Integer.valueOf(splits[2]);
+                String dividend = splits[3];
+
+                System.out.println(year + "/" + month + "/" + day + " -> " + dividend);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
